b95c8494666814d0eec50836fbb8c489c5895d6a
CYWangHome <chingyawang0513@gmail.com>
2026-07-09 00:35:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // 2. Paste the Web App URL below.
 // 3. Keep ADMIN_TOKEN the same as the ADMIN_TOKEN in Google Apps Script.
 window.CIDENG_CONFIG = {
-  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzDR-uZnVIzasy9T80sUQnh-YAD_uK6lvh_Fo6exXsbGcivSt_ip5m1COJ7ZxfNeg9a/exec',
+  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzD4AL3HVxkDlF_FXrmbCGYI6kl8KQgbeY5SGLBH-B5WYcNy5kUlXiytoT9NQUsiWRs/exec',
   ADMIN_TOKEN_STORAGE_KEY: 'cideng_admin_token'
 };
